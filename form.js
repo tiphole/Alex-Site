04194b08f32776ad8294ce9d42370a0e5d0e9459
tiphole <13-13816@saeinstitute.edu>
2024-03-11 00:00:01 +0200

    first micro Adjustments

--- form.js
+++ form.js
@@ -1,21 +1,14 @@
-const form = document.querySelector(".form");
-const email1 = document.querySelector("#email_input");
-const email2 = document.querySelector("#email_verify");
+const forms = document.getElementsByClassName("form");
+const email1 = document.getElementById("email-input");
+const email2 = document.getElementById("email-verify");
+console.log(forms)
+Array.from(forms).forEach(form => {
+    form.addEventListener('submit', handleSubmit);
+});
 
-form.addEventListener("submit", handleSubmit);
-email2.style.backgroundColor = "red";
-function handleSubmit() {
-
-    if (email1.value === email2.value) {
-        email2.style.background = "transparent";
-        alert("Your message has been sent succesfuly!")
-    }
-    else {
-        const email1 = document.querySelector("#email1");
-        const email2 = document.querySelector("#email2");
-        event.preventDefault();
-        email2.style.backgroundColor = "red";
-        email1.style.backgroundColor = "red";
-        alert("The first and second e-mail are not the same...  Please check again for any mistake!")
+function handleSubmit(event){
+    if(email1.value!==email2.value){
+        email2.style.background= 'red';
+        event.preventDefault() 
     }
 }
